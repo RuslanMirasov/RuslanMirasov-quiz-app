3adc2312854ba6989a2bdf75c06f56ebdadd5e84
Ruslan Mirasov <info@mirasov.dev>
2024-08-07 01:02:38 +0200

    change js logic on cards

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,76 +1,40 @@
-const refs = {
-  body: document.querySelector(".body"),
-  navLinks: document.querySelectorAll(".js-nav-link"),
-  themeSwitcher: document.querySelector("#theme-switcher"),
-  cards: document.querySelector(".cards"),
+const themaSwitcherEl = document.querySelector("#thema-switcher");
+const cardsEl = document.querySelector(".js-cards");
+
+const changeThema = (e) => {
+  const thema = e.target.checked ? "dark" : "light";
+  document.body.dataset.thema = thema;
+  localStorage.setItem("thema", thema);
 };
 
-// Set active link to menu
-const setActiveMenuLink = () => {
-  const pageUrl = window.location.href.split("/").pop();
-
-  if (!pageUrl) {
-    refs.navLinks[0].parentNode.classList.add("active");
-    return;
-  }
-
-  refs.navLinks.forEach((link) => {
-    const linkHref = link.href;
-    if (linkHref.includes(pageUrl)) {
-      link.parentNode.classList.add("active");
-      return;
-    }
-  });
-};
-
-// Theme switcher functions
-const switchThema = (e) => {
-  const newTheme = e.target.checked ? "dark" : "light";
-  refs.body.dataset.theme = newTheme;
-  localStorage.setItem("theme", newTheme);
-};
-
-const themeInit = () => {
-  const savedTheme = localStorage.getItem("theme");
-  const currentTheme = savedTheme || "light";
-  localStorage.setItem("theme", currentTheme);
-  refs.body.dataset.theme = currentTheme;
-  if (refs.themeSwitcher) {
-    refs.themeSwitcher.checked = currentTheme === "dark";
-    refs.themeSwitcher.addEventListener("change", switchThema);
+const themaInit = () => {
+  const currentThema = localStorage.getItem("thema") ?? "light";
+  document.body.dataset.thema = currentThema;
+  if (themaSwitcherEl) {
+    themaSwitcherEl.checked = currentThema === "dark";
   }
 };
 
-// Flip cards functions
-const flipCard = (e) => {
+const handleCardsClick = (e) => {
   const target = e.target;
 
-  if (
-    !target.classList.contains("button") &&
-    !target.classList.contains("add-to-bookmark")
-  )
-    return;
-
-  console.log(target);
-  if (target.classList.contains("add-to-bookmark")) {
-    target.classList.toggle("add-to-bookmark--active");
-    return;
-  }
-
-  const card = target.closest(".card");
-  if (target.classList.contains("js-card-show")) {
-    card.classList.add("active");
-    return;
-  }
-  card.classList.remove("active");
-};
-
-const flipCardInit = () => {
-  if (refs.cards) {
-    refs.cards.addEventListener("click", flipCard);
+  if (target.classList.contains("button")) {
+    target.closest(".card").classList.toggle("hidden");
+  } else if (target.classList.contains("add-to-bookmark")) {
+    target.classList.toggle("active");
   }
 };
 
-themeInit();
-setActiveMenuLink();
-flipCardInit();
+if (cardsEl) {
+  cardsEl.addEventListener("click", handleCardsClick);
+}
+
+if (themaSwitcherEl) {
+  themaSwitcherEl.addEventListener("change", changeThema);
+}
+
+const init = () => {
+  themaInit();
+};
+
+init();
